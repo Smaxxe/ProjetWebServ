1b8b8a7ecf6c87b055bf65da5dc95e8d32942b56
MaximePortable <maxime.sage97@gmail.com>
2022-03-19 12:07:23 +0100

    Fix des problèmes de styles de boutons

--- resources/views/adminseries/edit.blade.php
+++ resources/views/adminseries/edit.blade.php
@@ -1,61 +1,62 @@
 @extends('/layouts/main')
 
-{{--On est dans le menu d'édition/suppresion d'une série existante--}}
+{{-- On est dans le menu d'édition/suppresion d'une série existante --}}
 
 @section('content')
-    <h1>Modification / Suppression de la série <strong>{{$serie->title}}</strong> (ID : {{$serie->id}})</h1>
+    <h1>Modification / Suppression de la série <strong>{{ $serie->title }}</strong> (ID : {{ $serie->id }})</h1>
 
-    @if ($errors->any()) {{--Si la validation renvoie une erreur, on affiche ici--}}
-    <div class="" style="padding-top: 20px">
-        <ul style="color:red">
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+    @if ($errors->any())
+        {{-- Si la validation renvoie une erreur, on affiche ici --}}
+        <div class="" style="padding-top: 20px">
+            <ul style="color:red">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
     @endif
 
-    {{-- Modification de la série--}}
-    <form method="POST" action="/admin/series/{{$serie->id}}">
+    {{-- Modification de la série --}}
+    <form method="POST" action="/admin/series/{{ $serie->id }}">
         @method('PUT')
         @csrf
         <div>
             <label for="title" style="font-size: 15pt">Titre :</label>
-            <textarea name="title" id="title" style="resize: none">{{$serie->title}}</textarea>
+            <textarea name="title" id="title" style="resize: none">{{ $serie->title }}</textarea>
         </div>
-        <div> {{--Ici le choix de l'auteur se fait via un menu de sélection déjà rempli par les auteurs existants--}}
+        <div> {{-- Ici le choix de l'auteur se fait via un menu de sélection déjà rempli par les auteurs existants --}}
             <label for="author" style="font-size: 15pt">Auteur : </label>
             <select name="author" id="author">
-                <option>{{$serie->author->name}}</option> {{--En première option on place l'auteur de la série, pour éviter d'avoir un vide--}}
+                <option>{{ $serie->author->name }}</option> {{-- En première option on place l'auteur de la série, pour éviter d'avoir un vide --}}
                 @foreach ($authors as $author)
-                    <option>{{$author->name}}</option>
+                    <option>{{ $author->name }}</option>
                 @endforeach
             </select>
         </div>
         <div>
             <label for="content" style="font-size: 15pt">Contenu :</label>
-            <textarea name="content" id="content" style="height:300px;resize:none">{{$serie->content}}</textarea>
+            <textarea name="content" id="content" style="height:300px;resize:none">{{ $serie->content }}</textarea>
         </div>
         <div>
             <label for="acteurs" style="font-size: 15pt">Acteurs :</label>
-            <textarea name="acteurs" id="acteurs" style="resize: none;height:100px">{{$serie->acteurs}}</textarea>
+            <textarea name="acteurs" id="acteurs" style="resize: none;height:100px">{{ $serie->acteurs }}</textarea>
         </div>
         <div>
             <label for="tags" style="font-size: 15pt">Tags :</label>
-            <textarea name="tags" id="tags" style="resize: none;height:100px">{{$serie->tags}}</textarea>
+            <textarea name="tags" id="tags" style="resize: none;height:100px">{{ $serie->tags }}</textarea>
         </div>
 
         <button type="submit" class="bouton-simple">Mettre à jour et passer à la gestion des médias</button>
     </form>
 
-    {{-- Suppression de la série, c'est un formulaire sinon on ne peut pas utiliser la méthode delete--}}
-    <form method="POST" action="/admin/series/{{$serie->id}}" style="margin-top: 20px;">
+    {{-- Suppression de la série, c'est un formulaire sinon on ne peut pas utiliser la méthode delete --}}
+    <form method="POST" action="/admin/series/{{ $serie->id }}" style="margin-top: 20px;">
         @method("DELETE")
         @csrf
         <button type="submit" class="bouton-alerte">Supprimer la série</button>
     </form>
 
-    <div style="margin-bottom: 5%;margin-top : 20px"> {{--Pour retourner à l'index de adminséries facilement--}}
+    <div style="margin-bottom: 5%;margin-top : 20px"> {{-- Pour retourner à l'index de adminséries facilement --}}
         <a href="/admin/series">Revenir à l'index des séries</a>
     </div>
 @endsection
