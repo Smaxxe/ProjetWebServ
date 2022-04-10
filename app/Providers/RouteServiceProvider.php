c30fec05df01adf541b25e45907a6f73b28de12c
Guillaume <piolatg@gmail.com>
2022-04-10 16:32:01 +0200

    fix du redirect après s'etre enregistré

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/'; //HOME n'est plus la page dashboard, mais :  "/"
 
     /**
      * The controller namespace for the application.
